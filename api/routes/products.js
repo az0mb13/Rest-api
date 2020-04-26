6d869ff0c29b9a430428a82772ef60abdc9e4679
az0mb13 <qwertyuiop.dixit3@gmail.com>
2020-04-27 01:57:13 +0530

    Initial commit

---
+++ api/routes/products.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+    res.status(200).json({
+        message: 'Handling GET requests to /products'
+    });
+});
+
+router.post('/', (req, res, next) => {
+    const product = {
+        name: req.body.name,
+        price: req.body.price
+    };
+    res.status(201).json({
+        message: 'Handling POST requests to /products',
+        createdProduct: product
+    });
+});
+
+router.get('/:productId', (req, res, next) => {
+    const id = req.params.productId;
+    if (id === 'special'){
+        res.status(200).json({
+            message: 'Special ID',
+            id: id
+        });
+    } else {
+        res.status(200).json({
+            message: 'You passed an ID'
+        });
+    }
+});
+
+router.patch('/:productId', (req, res, next) => {
+    res.status(200).json({
+        message: 'Updated product!'
+    });
+});
+
+router.delete('/:productId', (req, res, next) => {
+    res.status(200).json({
+        message: 'Delete product!'
+    });
+});
+
+module.exports = router;
